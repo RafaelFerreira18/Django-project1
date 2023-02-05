080d25dc02ad59fca5893fe01aae0ee3cda96fce
PaçocasMultiplas <rafabf18@gmail.com>
2023-02-05 20:05:59 -0300

    removing examples

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home/home.html')
-
-
-def sobre(request):
-    return HttpResponse("Hello Django!")
-
-
-def contato(request):
-    return HttpResponse("contato")
+    return render(request, 'home.html', context={"name": "Rafael"})
