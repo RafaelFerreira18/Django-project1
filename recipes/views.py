218b31f59c2df8378abf1220d5ad683a7b8f0bf3
PaçocasMultiplas <rafabf18@gmail.com>
2023-02-05 21:28:23 -0300

    partials and website header

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', context={"name": "Rafael"})
+    return render(request, 'recipes/pages/home.html',
+                  context={"name": "Rafael"})
