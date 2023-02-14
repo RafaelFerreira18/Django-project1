25ef0a36994526cd2ede2cc8a07c3cd94866afc3
PaçocasMultiplas <rafabf18@gmail.com>
2023-02-14 16:08:14 -0300

    finishing the recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponse
 from django.urls import path
 
 from recipes.views import home
 
+
+def sobre(request):
+    return HttpResponse("Oi, Roberto!")
+
+
 urlpatterns = [
     path('', home),
+    path("sobre", sobre),
 
 ]
